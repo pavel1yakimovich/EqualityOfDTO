19bbfc61579043fa89bad1492bf1386d8c92a04d
pavel1yakimovich <pavel1yakimovich@gmail.com>
2016-11-07 16:40:13 +0300

    custom exception and xml comments for it added

--- ComparerLibrary/WrongAccuracyUsageException.cs
+++ ComparerLibrary/WrongAccuracyUsageException.cs
@@ -4,6 +4,10 @@ namespace ComparerLibrary
 {
     public class WrongAccuracyUsageException : Exception
     {
+        /// <summary>
+        /// Exception is thrown when AccuracyAttribute is used in a wrong way
+        /// </summary>
+        /// <param name="msg">message for debug</param>
         public WrongAccuracyUsageException(string msg = null) : base(msg)
         {
         }
