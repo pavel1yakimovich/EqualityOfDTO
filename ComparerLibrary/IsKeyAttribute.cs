ac7026feb8af9546d9d3ee256479b3dd4d0f5e5e
Pavel Yakimovich <Pavel_Yakimovich@epam.com>
2016-12-05 11:35:04 +0300

    iskey attribute added

---
+++ ComparerLibrary/IsKeyAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ComparerLibrary
+{
+    /// <summary>
+    /// Attribute for setting keys in collection
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class IsKeyAttribute : Attribute
+    {
+        public bool Flag { get; set; }
+
+        public IsKeyAttribute() : this(true)
+        {
+        }
+
+        public IsKeyAttribute(bool flag)
+        {
+            this.Flag = flag;
+        }
+    }
+}
