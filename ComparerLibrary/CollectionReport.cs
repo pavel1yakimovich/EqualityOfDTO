5aeca25b1417d6a1a77f9e473e6814660615abe2
Pavel Yakimovich <Pavel_Yakimovich@epam.com>
2016-12-05 11:30:37 +0300

    model for report of collection added

---
+++ ComparerLibrary/CollectionReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparerLibrary
+{
+    public class CollectionReport<T>
+    {
+        public IEnumerable<T> Added { get; set; }
+
+        public IEnumerable<T> Removed { get; set; }
+
+        public IDictionary<T, PropertyTree> Updated { get; set; }
+    }
+}
